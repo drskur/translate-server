b5b48a05165d6af3f9ec13e953677fe2d91255a7
ChangHun Yoon <drskur@amazon.com>
2022-04-15 22:19:13 +0900

    logging

--- translate/src/bin/app.rs
+++ translate/src/bin/app.rs
@@ -1,14 +1,17 @@
+use std::sync::Arc;
 use actix_web::dev::ServiceRequest;
-use actix_web::{Error, HttpResponse, Result};
+use actix_web::{Error, HttpResponse, Result, web};
 use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
 use actix_web_httpauth::extractors::basic::BasicAuth;
 use actix_web_httpauth::middleware::HttpAuthentication;
+use actix_web::middleware::Logger;
 use actix_web::http::header::ContentType;
 use actix_web::web::Json;
 use lambda_web::actix_web::{self, get, post, App, HttpServer};
 use lambda_web::{is_running_on_lambda, run_actix_on_lambda, LambdaError};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use env_logger::Env;
 
 async fn validator(req: ServiceRequest, credentials: BasicAuth) -> Result<ServiceRequest, Error> {
 
@@ -19,12 +22,32 @@ async fn validator(req: ServiceRequest, credentials: BasicAuth) -> Result<Servic
     Ok(req)
 }
 
+struct AppState {
+    shared_config: aws_types::SdkConfig
+}
+
+impl AppState {
+    async fn load_from_env() -> AppState {
+        let shared_config = aws_config::load_from_env().await;
+        AppState{
+            shared_config
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> Result<(),LambdaError> {
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let state = Arc::new(AppState::load_from_env().await);
+
     let factory = move || {
         let auth = HttpAuthentication::basic(validator);
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .wrap(auth)
+            .wrap(Logger::default())
             .service(index)
             .service(translate)
     };
@@ -55,9 +78,8 @@ struct TranslateMsg {
 }
 
 #[post("/api/translate")]
-async fn translate(msg: Json<TranslateMsg>) -> Result<HttpResponse, Error> {
-    let shared_config = aws_config::load_from_env().await;
-    let client = aws_sdk_translate::Client::new(&shared_config);
+async fn translate(msg: Json<TranslateMsg>, data: web::Data<Arc<AppState>>) -> Result<HttpResponse, Error> {
+    let client = aws_sdk_translate::Client::new(&data.shared_config);
 
     let mut lines = vec![];
 
