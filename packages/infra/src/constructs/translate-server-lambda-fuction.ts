2d4f34de1526014b89a01ae971b770e3ebd1dd9b
ChangHun Yoon <drskur@amazon.com>
2023-12-13 12:38:59 +0900

    fix: use lambda web adapter layer

--- packages/infra/src/constructs/translate-server-lambda-fuction.ts
+++ packages/infra/src/constructs/translate-server-lambda-fuction.ts
@@ -1,13 +1,14 @@
-import { Construct } from "constructs";
+import { Stage } from "aws-cdk-lib";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import {
   Architecture,
   Code,
   Function,
   IFunction,
+  LayerVersion,
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
-import { Stage } from "aws-cdk-lib";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
 
 export class TranslateServerLambdaFuction extends Construct {
   public readonly fn: IFunction;
@@ -15,12 +16,20 @@ export class TranslateServerLambdaFuction extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    const webAdapterLayer = LayerVersion.fromLayerVersionArn(
+      this,
+      "WebAdapterLayer",
+      `arn:aws:lambda:${Stage.of(this)
+        ?.region}:753240598075:layer:LambdaAdapterLayerArm64:17`,
+    );
+
     this.fn = new Function(this, "Function", {
       functionName: `TranslateServer-${Stage.of(this)?.stageName}`,
       runtime: Runtime.PROVIDED_AL2023,
       code: Code.fromAsset("../server/.dist/"),
       handler: "bootstrap",
       architecture: Architecture.ARM_64,
+      layers: [webAdapterLayer],
     });
 
     this.fn.addToRolePolicy(
--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -5,11 +5,10 @@ use std::sync::Arc;
 use actix_web::{App, HttpServer, web};
 use actix_web::middleware::Logger;
 use env_logger::{Env};
-use lambda_web::{is_running_on_lambda, LambdaError, run_actix_on_lambda};
 use crate::app_state::AppState;
 
 #[actix_web::main]
-async fn main() -> Result<(), LambdaError> {
+async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let state = Arc::new(AppState::load_from_env().await);
@@ -21,14 +20,10 @@ async fn main() -> Result<(), LambdaError> {
             .service(services::translate::handler)
     };
 
-    if is_running_on_lambda() {
-        run_actix_on_lambda(factory).await?;
-    } else {
-        HttpServer::new(factory)
-            .bind("127.0.0.1:8080")?
-            .run()
-            .await?;
-    }
+    HttpServer::new(factory)
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
 
     Ok(())
 }
